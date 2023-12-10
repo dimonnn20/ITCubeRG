c389c08cc1ffa9c7467581ae762fd85012503b31
dimonnn20 <dimonnn20@gmail.com>
2023-12-10 23:05:48 +0100

    Добавьте файлы проекта.

---
+++ ITCubeRG/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITCubeRG
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Choose_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = saveFileDialog.FileName;
+                PathToSaveBox.Text = selectedFilePath;
+                // Делайте что-то с выбранным путем сохранения файла
+                MessageBox.Show($"Выбран файл: {selectedFilePath}");
+            }
+        }
+
+        private void Generate_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
